8c17dd39333d70bf86555ee55e2d045c0a1089f9
juninhocb <juninhocb@hotmail.com>
2022-10-01 00:22:14 -0300

    start

---
+++ 12_/CRUD/src/Console_Main/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Main
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            
+
+
+        }
+    }
+}
